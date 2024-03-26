eb72e08d81106685043c1cec9ee9b08f50a83710
Cave <uriaranda27@gmail.com>
2024-03-25 18:28:17 -0600

    colores

--- forms/Login.py
+++ forms/Login.py
@@ -4,7 +4,7 @@ from wtforms.validators import Length, DataRequired
 
 
 class LoginForm(Form):
-    correo = StringField('correo', validators=[
+    correo = StringField('Correo', validators=[
         DataRequired(message="El correo es obligatorio."),
         Length(min=4, max=25, message="El correo debe tener entre 4 y 25 caracteres.")
     ], render_kw={"class": "input input-bordered w-full max-w-xs text-black"})
