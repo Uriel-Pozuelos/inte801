260c189234ea7e8134f21fbafce18352c4502a1e
IDGS-801-21000016 <u25936629@gmail.com>
2024-04-02 12:35:43 -0600

    🐛 fix (usuario) Fixxed integrity errors

--- routes/usuario.py
+++ routes/usuario.py
@@ -3,34 +3,51 @@ from models.usuario import Usuario,db
 from lib.jwt import allowed_roles, token_required,decodeToken
 from forms.Usuario import UsuarioForm
 from lib.d import D
+from sqlalchemy.exc import IntegrityError
+
 
 log = D(debug=True)
 
 usuario = Blueprint('usuario', __name__)
 
 def get_usuarios():
-    usuarios = Usuario.query.all()
+    usuarios = Usuario.query.filter_by(estado='activo').all()
     usuarios = [usuario.serialize() for usuario in usuarios]
+
     return usuarios
 
 def delete_usuario_by_id(id):
     usuario = Usuario.query.filter_by(id=id).first()
-    db.session.delete(usuario)
+    usuario.estado = 'inactivo'
     db.session.commit()
 
 
-def update_usuario_by_id(id,form):
+def update_usuario_by_id(id,form,role):
     usuario = Usuario.query.filter_by(id=id).first()
     usuario.nombre = form.nombre.data
     usuario.apellido = form.apellido.data
-    usuario.email = form.email.data
-    usuario.rol = form.rol.data
+
+    log.info(f'role: {usuario.rol} form: {form.rol.data}')
+
+    if usuario.rol == 'admin' and form.rol.data != 'admin':
+        return False
+    else:
+         usuario.rol = form.rol.data
+    if usuario.email != form.email.data:
+        usuario.email = form.email.data
     db.session.commit()
+    return True
 
 def add_usuario(form):
-    usuario = Usuario(nombre=form.nombre.data, apellido=form.apellido.data, email=form.email.data, password=form.password.data, rol=form.rol.data,estado='activo')
-    db.session.add(usuario)
-    db.session.commit()
+    try:
+        usuario = Usuario(nombre=form.nombre.data, apellido=form.apellido.data, email=form.email.data, password=form.password.data, rol=form.rol.data,estado='activo')
+        db.session.add(usuario)
+        db.session.commit()
+        return True
+    except IntegrityError as e:
+        log.error(e)
+        db.session.rollback()
+        return False
 
 
 
@@ -56,15 +73,16 @@ def index():
 
                 errors = [form.errors[field][0] for field in form.errors]
                 log.info(errors)
-            
                 return render_template('pages/usuario/index.html', form=form, usuarios=get_usuarios(), showModal='true', Mode='edit',errors=errors)
 
             token = decodeToken(request.cookies.get('token'))
-            if token['email'] == form.email.data:
-                flash('No puedes editar tu propio usuario')
+                
+            can_edit = update_usuario_by_id(form.id.data, form, token['role'])
+            if can_edit:
+                flash('Usuario editado correctamente')
+            else:
+                flash('No puedes cambiar el rol de un administrador', 'danger')
                 return render_template('pages/usuario/index.html', form=form, usuarios=get_usuarios(), showModal='false', Mode='edit', errors=None)
-            update_usuario_by_id(form.id.data, form)
-            flash('Usuario editado correctamente')
         elif 'add' in request.form:
             if not form.validate():
                 form.errors.pop('password')
@@ -75,8 +93,11 @@ def index():
                 errors = [form.errors[field][0] for field in form.errors]
                 log.info(errors)
                 return render_template('pages/usuario/index.html', form=form, usuarios=get_usuarios(), showModal='true', Mode='add', errors=errors)
-            add_usuario(form)
-            flash('Usuario agregado correctamente')
+            result = add_usuario(form)
+            if result:
+                flash('Usuario agregado correctamente')
+            else:
+                flash('El correo ya está en uso, intenta con otro')
         else:
             flash('Error al procesar la solicitud')
             
