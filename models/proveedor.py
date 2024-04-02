920de1e08ac5dd5eb3353ecf16c8d748e5e68b6c
IDGS-801-21000016 <u25936629@gmail.com>
2024-04-02 13:07:44 -0600

    🐛 fix (proveedor-materia) Fix relacionship

--- models/proveedor.py
+++ models/proveedor.py
@@ -1,4 +1,5 @@
 from db.db import db
+from sqlalchemy.orm import relationship
 
 class Proveedor(db.Model):
     __tablename__ = "proveedores"
@@ -7,7 +8,6 @@ class Proveedor(db.Model):
     direccion_empresa = db.Column(db.String(255), nullable=False)
     telefono_empresa = db.Column(db.String(13), nullable=False)
     nombre_atencion = db.Column(db.String(255), nullable=False)
-    productos = db.Column(db.String(255), nullable=False)
     estatus = db.Column(db.Integer, default=1)
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
@@ -20,9 +20,9 @@ class Proveedor(db.Model):
             'direccion_empresa': self.direccion_empresa,
             'telefono_empresa': self.telefono_empresa,
             'nombre_atencion': self.nombre_atencion,
-            'productos': self.productos,
             'estatus': self.estatus,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
-            'deleted_at': self.deleted_at
+            'deleted_at': self.deleted_at,
+            'materia_primas': [materia_prima.serialize() for materia_prima in self.materia_primas]
         }
