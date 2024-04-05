ae55b6c148980e481751264c41f5a9d4c4ef16ff
Fernando Valderrama <danimanri05@gmail.com>
2024-04-04 23:43:47 -0600

    Blue print produccion y solicitud

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ app.config.from_object(DevConfig)
 csrf = CSRFProtect(app)
 app.register_blueprint(recetas)
 app.register_blueprint(login)
+app.register_blueprint(produccion)
+app.register_blueprint(solicitud)
 
 @app.route("/b", methods=["GET", "POST"])
 @token_required
